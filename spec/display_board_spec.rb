4dbf7c8d9540c28b8634d5d5711f0ee0219fdeec
wesley perez <wesley.m.perez@gmail.com>
2019-11-21 19:55:11 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -175,7 +175,7 @@ describe "#display_board in 'lib/display_board.rb" do
       # *** Comment the line below by adding a # at the line start ***
       #expect(true).to be(true)
     end
-    
+
     it 'prints an entire board full of Os' do
       # Can you copy the syntax of the tests above to write a test for a board
       # entirely filled with Os?
@@ -183,7 +183,7 @@ describe "#display_board in 'lib/display_board.rb" do
 
       output = capture_puts{ display_board(board) } if defined?(display_board)
       rows = output.split("\n")
-      
+
       expect(rows[0]).to eq(" O | O | O ")
       expect(rows[1]).to eq("-----------")
       expect(rows[2]).to eq(" O | O | O ")
